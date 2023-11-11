a5412605193b6885c822e675438aec9a3496d767
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-11-11 20:48:16 +0530

    Let's Prectice Python

---
+++ CodeChef-Python/Burgers.py
@@ -0,0 +1,13 @@
+#Chef is fond of burgers and decided to make as many burgers as possible.
+#Chef has 
+
+A patties and 
+�
+B buns. To make 
+1
+1 burger, Chef needs 
+1
+1 patty and 
+1
+1 bun.
+Find the maximum number of burgers that Chef can make.
