c68e4ff6dcf9d06a08bc91aecff7cbe23e51efb4
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-11-23 23:48:37 +0530

    codechef 109 contest

---
+++ CodeChef-Python/Cricket World Cup Qualifier.py
@@ -0,0 +1,12 @@
+"""The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches. Any team that scores 
+12
+12 or more points in the group stage matches qualifies for the next stage.
+
+You know the score that a particular team has scored in the group stage matches. Determine if the team has qualified for the next stage or not."""
+
+# cook your dish here
+t=int(input())
+if t>=12 :
+    print("Yes")
+else:
+    print("No")
