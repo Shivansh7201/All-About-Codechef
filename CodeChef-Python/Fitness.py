7059b5bfbf35d135d1ace42c7d8814e4c7547dbf
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-11-27 23:55:11 +0530

    Create Fitness.py

---
+++ CodeChef-Python/Fitness.py
@@ -0,0 +1,11 @@
+""" Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is 
+X km away from his home.
+
+If his office is open on 
+5
+5 days in a week, find the number of kilometers Chef travels through office trips in a week. """
+
+t=int(input())
+for _ in range(t):
+    x= int(input())
+    print(10*x)
