3e046d2e39fd93ef0b65dd0f06d57fc7b4515496
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-12-13 22:33:00 +0530

    Add files via upload

---
+++ CodeChef-Python/FOODCOST.py
@@ -0,0 +1,6 @@
+# cook your dish here
+x,y=map(int,input().split()) # take input in two variables
+print(x*6 + y) #then as per the given condition multiple the first variable with # six (6) and after that add with the second variable and print the result
+
+
+"""like share & Subscribe"""
