76e0ab42a7afb9aca9c516a0c5ef13e5b8ed82fb
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-03 13:45:55 +0530

    Create Saving_Taxes.py

---
+++ CodeChef-Python/Saving_Taxes.py
@@ -0,0 +1,10 @@
+# cook your dish heret\
+t=int(input())
+for _ in range(t):
+    s,d=map(int,input().split())
+    print(s-d)
+    
+ #then as per the given condition multiple the first variable with # six (6) and after that add with the second variable and print the result
+
+
+"""like share & Subscribe"""
