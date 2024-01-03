9822d2a44b026640c572c40416885dddbf94de6f
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-03 13:53:01 +0530

    Create Master_chef_finals.py

---
+++ CodeChef-Python/Master_chef_finals.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t=int(input()) #taking input from user in t 
+for _ in range(t): #start iterate the t values
+    x=int(input()) #taking another input in x var
+    if(x>10):  #now apply conditins
+        print("NO")
+    else:
+        print("YES")
+"""like share & Subscribe"""
