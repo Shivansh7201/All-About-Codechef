ba43db0b98421d0c2034cb9a06e9fb7120207405
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-03 13:24:58 +0530

    Create Lucky_7.py

---
+++ CodeChef-Python/Lucky_7.py
@@ -0,0 +1,6 @@
+# cook your dish here
+a=input().strip() #taking string as an input and analysing the string
+
+lucky_letter=a[6] #now findout the 7th element from the given string 
+
+print(lucky_letter) #ready the output to print
