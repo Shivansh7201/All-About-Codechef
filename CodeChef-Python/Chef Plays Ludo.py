b36ba8efed27488d26a5cb7c5047df46b037c20b
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-05 22:43:20 +0530

    Create Chef Plays Ludo.py

---
+++ CodeChef-Python/Chef Plays Ludo.py
@@ -0,0 +1,14 @@
+# cook your dish here
+x=int(input())
+for _ in range(x):
+    y=int(input())
+    if(y>=6):
+        print("YES")
+    else:
+        print("NO")
+
+
+
+"""like share & Subscribe"""
+
+"""THANKYOU"""
