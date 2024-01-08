70506434200a3ec22074c31c2434e5ad1dbd19fa
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-08 22:40:29 +0530

    Create Greater Average.py

---
+++ CodeChef-Python/Greater Average.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    z,x,c=map(int,input().split())
+    if(z+x>2*c):
+        print("YES")
+    else:
+        print("NO")
+    
+
+
+"""like share & Subscribe"""
