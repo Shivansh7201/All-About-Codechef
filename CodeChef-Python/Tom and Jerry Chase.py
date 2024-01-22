db9c37f5e597a8312f1399b72f7de18fca00f766
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-22 23:36:59 +0530

    Create Tom and Jerry Chase.py

---
+++ CodeChef-Python/Tom and Jerry Chase.py
@@ -0,0 +1,17 @@
+t=int(input())
+
+for _ in range(t):
+
+    x,c=map(int,input().split())
+
+    if(x>=c):
+
+        print("NO")
+
+    else:
+
+        print("YES")
+
+
+
+"""like share & Subscribe"""
