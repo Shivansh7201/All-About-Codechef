02ad2f6ab3193f1602e921cb6b795aa53c030327
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-30 22:59:06 +0530

    Create Exams.py

---
+++ CodeChef-Python/Exams.py
@@ -0,0 +1,17 @@
+t=int(input())
+
+for _ in range(t):
+
+    z,x,c=map(int,input().split())
+
+    if(c>(z*x)/2):
+
+        print("YES")
+
+    else:
+
+        print("NO")
+
+
+
+"""like share & Subscribe"""
