928e617a03c993da11a1bfa70360944cc7f8da0b
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-02-01 22:08:28 +0530

    Create Water Filling.py

---
+++ CodeChef-Python/Water Filling.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    l=list(map(int,input().split()))
+    num=sum(l)
+    if(num<=1):
+        print("Water filling time")
+    else:
+        print("Not now")
+"""like share & Subscribe"""
