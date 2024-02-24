a230f42f98448326afc23b7262696c2a50f14ad7
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-02-25 00:02:05 +0530

    Create Chef and nextgen.py.

---
+++ CodeChef-Python/Chef and nextgen.py
@@ -0,0 +1,7 @@
+for _ in range(int(input()): """ In this line of python script the user provide the input then onlly the iteration go with the flow"""
+  x,c,v,b=map(int,input().split()) """taking input in all the define variable"""
+  if((x*c)<=(v*b)): """ applying conditions """
+    print("Yes")
+  else:                  """Final Output here we get"""
+    print("No")
+  
