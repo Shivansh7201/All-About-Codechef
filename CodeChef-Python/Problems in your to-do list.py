6148698a59af8dbb54779de323800d0b0ba0febd
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-02-27 21:09:11 +0530

    Create Problems in your to-do list.py

---
+++ CodeChef-Python/Problems in your to-do list.py
@@ -0,0 +1,15 @@
+t=int(input())
+for _ in range(t):
+    q=int(input())
+    l=list(map(int,input().split()))
+    count=0
+    for i in range(q):
+        if(l[i]>=1000):
+            count+=1
+    print(count)    
+        
+        
+    
+    
+
+"""like share & Subscribe"""
