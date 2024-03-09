8e45c946d37c37ae9ff78b7c6901ca1e0a8b4e63
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-03-09 19:58:40 +0530

    Create Sasta shark tank.py

---
+++ CodeChef-Python/Sasta shark tank.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    a,s=map(int,input().split())
+    if(2*a>s):
+        print("FIRST")
+    elif(2*a<s):
+        print("SECOND")
+    else:
+        print("ANY")
+
+"like share & Subscribe"""
