dc4cf33e2d3a191e34a51f52bd15c34e7c5ad85b
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-03-09 20:04:51 +0530

    Create Qualify the round.py

---
+++ CodeChef-Python/Qualify the round.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    z,x,c=map(int,input().split())
+    if((x*1+c*2)>=z):
+        print("Qualify")
+    else:
+        print("NotQualify")
+
+"""like share & Subscribe"""
