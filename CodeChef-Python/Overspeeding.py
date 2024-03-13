e7b9db8ff7d61bc665dee2662eb1e6e0fea2bd49
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-03-13 22:13:23 +0530

    Create Overspeeding.py

---
+++ CodeChef-Python/Overspeeding.py
@@ -0,0 +1,11 @@
+t=int(input())
+if(t>40):
+    print("YES")
+else:
+    print("NO")
+
+"""The first line of code takes the input in the integer-type variable name (t) as well as declares it.
+
+Then, in the second line, apply the condition.
+
+Last but not least, we get the output."""
