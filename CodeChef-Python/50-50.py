8cbbe4a917f46520fbbc6ebf3af8af6303d99c2b
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-03-14 13:50:45 +0530

    Update 50-50.py

--- CodeChef-Python/50-50.py
+++ CodeChef-Python/50-50.py
@@ -1,1 +1,11 @@
+t=int(input())
+for _ in range(t):
+    a,s=map(int,input().split())
+    if(a<50):
+        print("Z")
+    elif(s<50 and a>=50):
+        print("F")
+    else:
+        print("A")
 
+"""like share & Subscribe"""
