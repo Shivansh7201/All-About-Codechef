dd05f89ef7f15ebd54b41c7603f2098bd7c128b3
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-04-02 12:09:03 +0530

    Create Bus_Seat_Numbering.py

---
+++ CodeChef-Python/Bus_Seat_Numbering.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    z=int(input())
+    if(z<=10):
+        print("Lower Double")
+    elif(10<z<=15):
+        print("Lower Single")
+    elif(25<z<=30):
+        print("Upper Single")
+    else:
+        print("Upper Double")
+
+"""like share & Subscribe"""
